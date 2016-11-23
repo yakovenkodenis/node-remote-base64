40ec510fe14f0549c5e4f90f3c1dd5156baacdb6
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-11-24 00:56:23 +0200

    Set up istanbul and Coveralls

--- test/test.js
+++ test/test.js
@@ -8,4 +8,8 @@ describe('#remoteBase64', function() {
     it('should exist', function() {
         expect(remoteBase64).to.exist;
     });
+
+    it('should be ok', function() {
+        expect(remoteBase64).to.be.ok;
+    })
 });
